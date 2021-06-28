35187e3b901e595687498c17c676bc432c4d92a2
Mohamed Amr Salem <mohamed.amr.salem@gmail.com>
2021-06-28 22:54:20 +0200

    added auth module

--- app/Client.php
+++ app/Client.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    protected $fillable = ['full_name', 'mobile', 'email'];
+    public function invoices()
+    {
+        $this->hasMany(Invoice::class);
+    }
 }
